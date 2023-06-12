7fdca75cb9eff7fab3a2097efcc3b7fa77aac0f2
bore <ibrakhimov2005@gmail.com>
2023-06-12 22:57:09 +0600

    first commit

--- src/common/header/Search.jsx
+++ src/common/header/Search.jsx
@@ -1,33 +1,40 @@
-import React from "react"
-import logo from "../../components/assets/images/logo.svg"
-import { Link } from "react-router-dom"
+import React from "react";
+import logo from "../../components/assets/images/63ef8c24f4d39da7df4eafea_Screenshot 2023-02-17 at 8.28.17 PM.png";
+import { Link } from "react-router-dom";
 
 const Search = ({ CartItem }) => {
   // fixed Header
   window.addEventListener("scroll", function () {
-    const search = document.querySelector(".search")
-    search.classList.toggle("active", window.scrollY > 100)
-  })
+    const search = document.querySelector(".search");
+    search.classList.toggle("active", window.scrollY > 100);
+  });
 
   return (
     <>
-      <section className='search'>
-        <div className='container c_flex'>
-          <div className='logo width '>
-            <img src={logo} alt='' />
+      <section className="search">
+        <div className="container c_flex">
+          <div className="logo width">
+            <img
+              src={logo}
+              alts=""
+              style={{
+                width: "50%",
+              }}
+            />
+            {/* Online Store */}
           </div>
 
-          <div className='search-box f_flex'>
-            <i className='fa fa-search'></i>
-            <input type='text' placeholder='Search and hit enter...' />
+          <div className="search-box f_flex">
+            <i className="fa fa-search"></i>
+            <input type="text" placeholder="Search and hit enter..." />
             <span>All Category</span>
           </div>
 
-          <div className='icon f_flex width'>
-            <i className='fa fa-user icon-circle'></i>
-            <div className='cart'>
-              <Link to='/cart'>
-                <i className='fa fa-shopping-bag icon-circle'></i>
+          <div className="icon f_flex width">
+            <i className="fa fa-user icon-circle"></i>
+            <div className="cart">
+              <Link to="/cart">
+                <i className="fa fa-shopping-bag icon-circle"></i>
                 <span>{CartItem.length === 0 ? "" : CartItem.length}</span>
               </Link>
             </div>
@@ -35,7 +42,7 @@ const Search = ({ CartItem }) => {
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
